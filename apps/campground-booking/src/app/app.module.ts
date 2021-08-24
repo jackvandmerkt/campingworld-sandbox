be82dcf81c41d91fc415b1a07f27fb46c9e2853f
jcarlsso-apt <james.carlsson@aptitive.com>
2021-08-24 14:37:03 -0500

    adding listings and orders cards

--- apps/campground-booking/src/app/app.module.ts
+++ apps/campground-booking/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListingsComponent } from './home/cards/listing.component';
+import { OrdersComponent } from './home/cards/orders.component';
+import { CombinedCardComponent } from './home/combined-cards.component';
 import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    NavComponent
+    NavComponent,
+    OrdersComponent,
+    ListingsComponent,
+    CombinedCardComponent
   ],
   imports: [BrowserModule],
   providers: [],
