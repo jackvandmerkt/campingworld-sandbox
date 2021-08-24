3195355a949c00c4248d14903b54937ea4133a62
Jack Vandemerkt <61991993+jackvandmerkt@users.noreply.github.com>
2021-08-24 10:38:53 -0500

    initial commit

---
+++ apps/campground-booking/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'campingworld-sandbox-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'campground-booking';
+}
