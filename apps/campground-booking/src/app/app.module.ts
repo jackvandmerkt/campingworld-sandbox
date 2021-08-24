62d119d6d984dbe9bd1dc33cf19e8346efbcfcb7
jcarlsso-apt <james.carlsson@aptitive.com>
2021-08-24 12:00:37 -0500

    adding tailwind

--- apps/campground-booking/src/app/app.module.ts
+++ apps/campground-booking/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    NavComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.js'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        cw: ['Montserrat'],
+        lato: ['Lato']
+      },
+      colors: {
+        cwRed: '#C30B24',
+        cwGray: '#474342',
+        cwLightGray: '#757272',
+        cwBg: "#E5E5E5",
+        formBlack: '#212B36'
+      },
+      spacing: {
+        '6.5': '26px',
+        '13': '52px',
+        '13.5': '55px',
+        '15': '60px',
+        '34': '136px',
+        '110': '110px',
+        '116': '116.41px',
+        '584': '584px',
+        '636': '636px',
+        '454': '454px',
+        '416': '416px',
+        '200': '200px'
+
+      },
+      fontSize: {
+        'nav': ['12px', '16px'],
+        'newListing': ['32px', '38px'],
+        'floatLabel': ['12px', '18px'],
+        'evaluationLabel': ['16px', '26px']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
+}
