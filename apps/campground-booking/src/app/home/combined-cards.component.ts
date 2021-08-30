3a3865840639f87eee7afd4bbe4823474dc25207
jcarlsso-apt <james.carlsson@aptitive.com>
2021-08-30 14:52:02 -0500

    renamed home page component and added updates

--- apps/campground-booking/src/app/home/combined-cards.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "combined-card",
-    templateUrl: "combined-cards.component.html"
-})
-export class CombinedCardComponent {
-
-}
--- apps/campground-booking/src/app/routes.ts
+++ apps/campground-booking/src/app/routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from "@angular/router";
 import { CreateListingNavComponent } from "./create-listing/create-listing-nav/create-listing-nav.component";
-import { CombinedCardComponent } from "./home/combined-cards.component";
+import { HomeComponent } from "./home/home.component";
 import { NewListingsComponent } from "./new-listing/new-listing.component";
 
 export const routes:Routes = [
-    { path: 'home', component: CombinedCardComponent},
+    { path: 'home', component: HomeComponent},
     { path: 'new-listing', component: NewListingsComponent },
     { path: 'create-listing-nav', component: CreateListingNavComponent },
     { path: '',   redirectTo: '/home', pathMatch: 'full'},
-    { path: '**', component: CombinedCardComponent}
+    { path: '**', component: HomeComponent}
 ];
