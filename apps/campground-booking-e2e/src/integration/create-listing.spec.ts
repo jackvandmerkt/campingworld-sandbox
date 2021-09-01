1e85b50d60cc756002919d8d495d9de5a5c31b01
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-01 16:31:53 -0500

    create listing cypress

--- apps/campground-booking-e2e/src/integration/create-listing.spec.ts
+++ apps/campground-booking-e2e/src/integration/create-listing.spec.ts
@@ -4,7 +4,18 @@ describe('campground-booking', () => {
     beforeEach(() => cy.visit('/create-listing'));
     it('should render components', () => {
         getText().contains('Need Support?');
+        getText().contains('Pro Tips');
+        getText().contains('Good Sam Rating');
+    });
+
+    it('Should expand all divs once expand all button is clicked', () => {
+        cy.get("button[data-expandAll=Submit]").click();
         getText().contains('Good Sam Record ID');
+        getText().contains('Interior Roads & Site Info');
+        getText().contains('Restrooms');
+        getText().contains('Tenting & Rentals');
+        getText().contains('Ratings');
+        cy.get("button[data-expandAll=Submit]").click();
     });
 
 });
