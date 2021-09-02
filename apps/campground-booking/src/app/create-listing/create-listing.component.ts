9b5e3312b5150df8866c8f46da0b5a1f6764b745
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-02 14:24:31 -0500

    ipad responsive layout

--- apps/campground-booking/src/app/create-listing/create-listing.component.ts
+++ apps/campground-booking/src/app/create-listing/create-listing.component.ts
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
 })
 export class CreateListingComponent {
 
-  locationInfoStep = 'Contact Info';
+  locationInfoStep = 'Good Sam Record ID';
 
   constructor() {
 
