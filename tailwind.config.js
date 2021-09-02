0ab791b8dd31ae91efbdc6c248e82c931128a0a6
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-02 11:04:56 -0500

    fixed shadows and added validate and submit

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,9 +24,10 @@ module.exports = {
         proposalsGray: '#EAECEF',
         cwBg: "#E5E5E5",
         formBlack: '#212B36',
-        lightOrange: '#FF974580',
+        lightOrange: '#FF9745',
         lightPurple: '#9B6CFC66',
-        lightGreen: '#50B83C80'
+        lightGreen: '#50B83C80',
+        validate: '#89C1B9'
       },
       opacity: {
         '35': '.35'
@@ -75,15 +76,8 @@ module.exports = {
       }
     },
     boxShadow: {
-        sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-        DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-        md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-        lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-        xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-        '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-       '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-        inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-        none: 'none'
+        cw: '0px 6px 16px rgba(0, 0, 0, 0.25)',
+        '8px32px': '0px 8px 32px rgba(0, 0, 0, 0.25)',
     }
   },
   variants: {
