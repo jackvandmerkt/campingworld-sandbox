f2c00e7b48c51290aeb3b277daa193c2cac33da6
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-08 13:50:38 -0500

    good sam park form

--- apps/campground-booking/src/app/routes.ts
+++ apps/campground-booking/src/app/routes.ts
@@ -3,6 +3,7 @@ import { CreateListingComponent } from "./create-listing/create-listing.componen
 import { ContactInfoComponent } from "./create-listing/location-info/contact-info/contact-info.component";
 import { DirectionsComponent } from "./create-listing/location-info/directions/directions.component";
 import { DiscountsAffiliationsComponent } from "./create-listing/location-info/discounts-affiliations/discounts-affiliations.component";
+import { GoodSamParkComponent } from "./create-listing/location-info/good-sam-park/good-sam-park.component";
 import { GoodSamRecordFormComponent } from "./create-listing/location-info/good-sam-record/good-sam-record.component";
 import { OwnerAndB2bInfoComponent } from "./create-listing/location-info/owner-and-b2b-info/owner-and-b2b-info.component";
 import { HomeComponent } from "./home/home.component";
@@ -17,7 +18,8 @@ export const routes:Routes = [
             { path: 'contact-info', component: ContactInfoComponent },
             { path: 'discounts-affiliations', component: DiscountsAffiliationsComponent },
             { path: 'owner-b2b-info', component: OwnerAndB2bInfoComponent },
-            { path: 'directions', component: DirectionsComponent }
+            { path: 'directions', component: DirectionsComponent },
+            { path: 'good-sam-park', component: GoodSamParkComponent }
         ] },
     { path: '',   redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: HomeComponent}
