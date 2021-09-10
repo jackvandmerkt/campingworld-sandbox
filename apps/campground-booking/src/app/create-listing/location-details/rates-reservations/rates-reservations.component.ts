57e9df470330d543f326093820685ac87213a156
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-10 15:57:42 -0500

    rates reservations form

--- apps/campground-booking/src/app/create-listing/location-details/rates-reservations/rates-reservations.component.ts
+++ apps/campground-booking/src/app/create-listing/location-details/rates-reservations/rates-reservations.component.ts
@@ -8,6 +8,17 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class RatesReservationsComponent implements OnInit{
     ratesReservations!: FormGroup;
+    creditCardsAccepted: boolean = false;
+    reservationsAccepted: boolean = false;
+    onlineReservation: boolean = false;
+    // credit cards checkbox booleans
+    debitSelected: boolean = false;
+    discoverSelected: boolean = false;
+    visaSelected: boolean = false;
+    mastercardSelected: boolean = false;
+    amexSelected: boolean = false;
+    // used for input box to only appear if user selects other in online reservation system dropdown
+    onlineReservationOther: boolean = false;
 
     constructor() {
 
@@ -19,4 +30,40 @@ export class RatesReservationsComponent implements OnInit{
         });
     }
 
+    checkBoxCreditCardsAcceptChange(cb:any) {
+        this.creditCardsAccepted = !this.creditCardsAccepted;
+    }
+    checkBoxReservationsAcceptChange(cb:any) {
+        this.reservationsAccepted = !this.reservationsAccepted;
+    }
+    checkBoxOnlineReservationChange(cb:any) {
+        this.onlineReservation = !this.onlineReservation;
+    }
+
+    creditCardChecked(radio: any) {
+        if(radio === "debit") {
+            this.debitSelected = !this.debitSelected;
+        }
+        if(radio === "discover") {
+            this.discoverSelected = !this.discoverSelected;
+        }
+        if(radio === "visa") {
+            this.visaSelected = !this.visaSelected;
+        }
+        if(radio === "mastercard") {
+            this.mastercardSelected = !this.mastercardSelected;
+        }
+        if(radio === "amex") {
+            this.amexSelected = !this.amexSelected;
+        }
+    }
+
+    onChange(event:any): void {
+        const newVal = event.target.value;
+        if(newVal === "other") {
+            this.onlineReservationOther = true;
+        } else {
+            this.onlineReservationOther = false;
+        }
+    }
 }
