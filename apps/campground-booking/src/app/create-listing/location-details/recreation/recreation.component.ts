5d0191f734b0331215a937373fd5bbd7e6821877
OFRA <franklin.lima-ext@hexagon.com>
2021-09-13 15:55:40 -0300

    feat: start recreation component with checkboxes

--- apps/campground-booking/src/app/create-listing/location-details/recreation/recreation.component.ts
+++ apps/campground-booking/src/app/create-listing/location-details/recreation/recreation.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecreationComponent implements OnInit {
 
+  option: string[] = [];
+  isRecreationChecked = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  optionChecked(radio: string) {
+    if (!this.option.includes(radio)) {
+      this.option.push(radio)
+    } else {
+      this.option = this.option.filter(option => option !== radio)
+    }
+
+  }
+
+  checkBoxRecreationChange(cb: any) {
+    this.isRecreationChecked = !this.isRecreationChecked;
+  }
 }
