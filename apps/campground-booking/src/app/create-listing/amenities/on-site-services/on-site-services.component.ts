8c4e8d56da35f3fbb61e827d22a4705f3659529a
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-14 11:54:19 -0500

    on site services form

--- apps/campground-booking/src/app/create-listing/amenities/on-site-services/on-site-services.component.ts
+++ apps/campground-booking/src/app/create-listing/amenities/on-site-services/on-site-services.component.ts
@@ -8,13 +8,25 @@ import { FormGroup, FormControl } from "@angular/forms";
 })
 export class OnSiteServicesComponent implements OnInit {
     onSiteServices!: FormGroup;
+    // radio checkbox variables
     basicFacilitiesOptions: string[] = [];
     securityOptions: string[] = [];
     otherServicesOptions: string[] = [];
+    petServicesOptions: string[] = [];
+
+    // toggle button boolean variables
     centralInternet: boolean = false;
+    centralInternetCharge: boolean = false;
     wifiHotspots: boolean = false;
+    wifiHotspotsCharge: boolean = false;
     wifi: boolean = false;
+    wifiCharge: boolean = false;
+    mobileDevices: boolean = false;
+    streamingSupport: boolean = false;
+    wiFiTechSupport: boolean = false;
     overnightCentralInternet: boolean = false;
+    overnightCentralInternetCharge: boolean = false;
+
     constructor() {
 
     }
@@ -49,16 +61,49 @@ export class OnSiteServicesComponent implements OnInit {
         }
     }
 
+    petServicesChecked(radio: string) {
+      if (!this.petServicesOptions.includes(radio)) {
+        this.petServicesOptions.push(radio)
+      } else {
+        this.petServicesOptions = this.petServicesOptions.filter(option => option !== radio)
+      }
+  }
+
     checkBoxCentralInternetChange(cb:any) {
-        this.centralInternet = !this.centralInternet;
+      this.centralInternet = !this.centralInternet;
     }
+    checkBoxCentralInternetChargeChange(cb:any) {
+      this.centralInternetCharge = !this.centralInternetCharge;
+    }
+
     checkBoxWiFiHotspotsChange(cb:any) {
-        this.wifiHotspots = !this.wifiHotspots;
+      this.wifiHotspots = !this.wifiHotspots;
     }
+    checkBoxWiFiHotspotsChargeChange(cb:any) {
+      this.wifiHotspotsCharge = !this.wifiHotspotsCharge;
+    }
+
     checkBoxWiFiChange(cb:any) {
-        this.wifi = !this.wifi;
+      this.wifi = !this.wifi;
     }
+    checkBoxWiFiChargeChange(cb:any) {
+      this.wifiCharge = !this.wifiCharge;
+    }
+
+    checkBoxMobileDevicesChange(cb:any) {
+      this.mobileDevices = !this.mobileDevices;
+    }
+    checkBoxStreamingSupportChange(cb:any) {
+      this.streamingSupport = !this.streamingSupport;
+    }
+    checkBoxWiFiTechSupportChange(cb:any) {
+      this.wiFiTechSupport = !this.wiFiTechSupport;
+    }
+
     checkBoxOvernightCentralInternetChange(cb:any) {
-        this.overnightCentralInternet = !this.overnightCentralInternet;
+      this.overnightCentralInternet = !this.overnightCentralInternet;
+    }
+    checkBoxOvernightCentralInternetChargeChange(cb:any) {
+      this.overnightCentralInternetCharge = !this.overnightCentralInternetCharge;
     }
 }
