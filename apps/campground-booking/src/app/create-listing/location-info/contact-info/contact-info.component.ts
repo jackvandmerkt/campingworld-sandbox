84b35fd819c33248e5118eae5fa58b011aeaaef2
OFRA <franklin.lima-ext@hexagon.com>
2021-09-20 13:56:39 -0300

    feat: basic form group

--- apps/campground-booking/src/app/create-listing/location-info/contact-info/contact-info.component.ts
+++ apps/campground-booking/src/app/create-listing/location-info/contact-info/contact-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'contact-info',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  contactInfoForm = this.formBuilder.group({
+    latitude: '',
+    longitude: '',
+    elevation: '',
+    email: '',
+    telephone: '',
+    repressedTelephone: '',
+    webAddress: '',
+    onlineWeb: '',
+    skipUtm: '',
+    facebook: '',
+    twitter: '',
+    pinterest: '',
+    instagram: '',
+  });
 
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+  onSubmit(): void {
+    console.log(this.contactInfoForm.value);
   }
 
 }
