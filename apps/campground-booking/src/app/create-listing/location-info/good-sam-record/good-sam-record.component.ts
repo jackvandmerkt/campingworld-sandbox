b8fb9202596265f1cd554c6913252b0f330a29c2
jcarlsso-apt <james.carlsson@aptitive.com>
2021-09-22 16:46:37 -0500

    good sam park record form validation start

--- apps/campground-booking/src/app/create-listing/location-info/good-sam-record/good-sam-record.component.ts
+++ apps/campground-booking/src/app/create-listing/location-info/good-sam-record/good-sam-record.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 import { ListingService } from "../../../shared/listing.service";
 
 @Component({
@@ -12,6 +12,8 @@ export class GoodSamRecordFormComponent implements OnInit{
     isGuestsChecked:boolean = false;
     isDeleteChecked:boolean = false;
     territories:any = [];
+    // temporary number, will use getter method to pull this number from api in the future
+    fileNum:any = '1230405060'
     constructor(private formBuilder: FormBuilder, private ls: ListingService) {
 
     }
@@ -28,21 +30,26 @@ export class GoodSamRecordFormComponent implements OnInit{
     }
 
     goodSamRecordForm = this.formBuilder.group({
-        parkName: '',
-        fileNum: '',
-        sectionCode: '',
-        repCode: '',
-        listCity: '',
-        listState: '',
-        territory: '',
-        listType: '',
-        parkType: '',
+        parkName: ['', Validators.required],
+        fileNum: [{value: this.fileNum, disabled: true},  Validators.required],
+        sectionCode: ['', Validators.required],
+        repCode: ['', Validators.required],
+        listCity: ['', Validators.required],
+        listState: ['', Validators.required],
+        territory: ['', Validators.required],
+        listType: ['', Validators.required],
+        parkType: ['', Validators.required],
         toggleGuests: false,
         toggleDelete: false
       });
     
     onSubmit(): void {
-        console.log(this.goodSamRecordForm.value);
+        if(this.goodSamRecordForm.valid) {
+            console.log(this.goodSamRecordForm.value);
+        } else {
+            console.log('not valid')
+        }
+        
     }
     clearChanges() {
         this.goodSamRecordForm.reset();
