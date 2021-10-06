e74686adda3aa132452d8aa46b319c2c00fb7fd1
jcarlsso-apt <james.carlsson@aptitive.com>
2021-10-06 15:31:58 -0500

    published ratings form validation

--- apps/campground-booking/src/app/create-listing/ratings/published-ratings/published-ratings.component.ts
+++ apps/campground-booking/src/app/create-listing/ratings/published-ratings/published-ratings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'published-ratings',
@@ -8,28 +8,39 @@ import { FormBuilder, FormGroup } from "@angular/forms";
 })
 
 export class PublishedRatingsComponent {
+    submitted: boolean = false;
     constructor(private formBuilder: FormBuilder) {
 
     }
 
     publishedRatings = this.formBuilder.group({
-        lyNonRatedCode: '',
-        lyFacilitiesTotal: '',
-        lyRestroomsTotal: '',
-        lyVisualTotal: '',
-        lyRatingChange: '',
-        tyNonRatedCode: '',
-        tyFacilitiesTotal: '',
-        tyRestroomsTotal: '',
-        tyVisualTotal: '',
-        tyRatingChange: '',
+        lyNonRatedCode: ['', Validators.required],
+        lyFacilitiesTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        lyRestroomsTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        lyVisualTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        lyRatingChange: ['', Validators.required],
+        tyNonRatedCode: ['', Validators.required],
+        tyFacilitiesTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        tyRestroomsTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        tyVisualTotal: ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        tyRatingChange: ['', Validators.required],
         confirmCheck: false
     });
 
     onSubmit(): void {
-        console.log(this.publishedRatings.value);
+        this.submitted = true;
+        if(this.publishedRatings.valid) {
+            console.log(this.publishedRatings.value);
+        } else {
+            console.log('not valid');
+            return;
+        }
     }
+    
+    get f() { return this.publishedRatings.controls; }
+
     clearChanges() {
         this.publishedRatings.reset();
+        this.submitted = false;
     }
 }
