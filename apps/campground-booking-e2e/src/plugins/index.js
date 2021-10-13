2fb97034adaa06ed5badef5d7ba2721eec791b2b
FranklinLima7 <franklin.lima@aptitive.com>
2021-10-13 14:40:34 -0300

    feat: add code coverage plugin

--- apps/campground-booking-e2e/src/plugins/index.js
+++ apps/campground-booking-e2e/src/plugins/index.js
@@ -16,7 +16,9 @@ const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor');
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require('@cypress/code-coverage/task')(on, config)
 
   // Preprocess Typescript file using Nx helper
   on('file:preprocessor', preprocessTypescript(config));
+  return config
 };
