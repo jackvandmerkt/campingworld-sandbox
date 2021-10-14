f03641b8a80a24c96ae0c75812e856af23ec2dc4
jcarlsso-apt <james.carlsson@aptitive.com>
2021-10-14 15:44:35 -0500

    orders card update

--- apps/campground-booking/src/app/home/home.component.ts
+++ apps/campground-booking/src/app/home/home.component.ts
@@ -29,6 +29,7 @@ export class HomeComponent implements OnInit{
             this.selectedTerritory = "All";
         } else {
             this.selectedTerritory = newVal;
+            console.log(this.selectedTerritory)
         }
     }
 
