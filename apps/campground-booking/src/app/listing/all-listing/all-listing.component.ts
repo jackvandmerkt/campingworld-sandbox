a16186abcb1da3001e7133523d2b6891b3a67a71
FranklinLima7 <franklin.lima@aptitive.com>
2021-10-18 16:11:23 -0300

    feat: filter bar

--- apps/campground-booking/src/app/listing/all-listing/all-listing.component.ts
+++ apps/campground-booking/src/app/listing/all-listing/all-listing.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'all-listing',
-    templateUrl: './all-listing.component.html',
-    styleUrls: ['./all-listing.component.css']
+  selector: 'all-listing',
+  templateUrl: './all-listing.component.html',
+  styleUrls: ['./all-listing.component.css']
 })
 export class AllListingsComponent {
+
+  onChange(): void {
+    console.log('aa')
+  }
 
 }
