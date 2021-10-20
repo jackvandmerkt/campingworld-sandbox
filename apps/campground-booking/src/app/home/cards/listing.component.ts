f5588856928de9092b0b3be06393415c6942196a
FranklinLima7 <franklin.lima@aptitive.com>
2021-10-20 18:37:26 -0300

    feat: working spinner and logic

--- apps/campground-booking/src/app/home/cards/listing.component.ts
+++ apps/campground-booking/src/app/home/cards/listing.component.ts
@@ -1,31 +1,41 @@
-import {Component, Input, OnChanges, OnInit } from "@angular/core";
+import { Component, Input, Output, OnChanges, OnInit, EventEmitter } from "@angular/core";
 import { Router } from "@angular/router";
-import {IListingCounts} from '../../shared/listing-counts.model';
-import {ListingService} from '../../shared/listing.service';
+import { IListingCounts } from '../../shared/listing-counts.model';
+import { ListingService } from '../../shared/listing.service';
 
 @Component({
-    selector: 'listings',
-    templateUrl: './listings.component.html'
+  selector: 'listings',
+  templateUrl: './listings.component.html'
 })
-export class ListingsComponent implements OnInit, OnChanges {
-    listingCounts!:IListingCounts;
-    @Input() territoryCode: string = '';
-    constructor(private router: Router, private listingService:ListingService) {
-    }
-    redirect() {
-        this.router.navigateByUrl('/new-listing')
-    }
-    ngOnInit(){
-        this.listingService.getListings('All').subscribe(response => {
-            this.listingCounts = response;
-       })
-    }
 
-    ngOnChanges() {
-        this.listingService.getListings(this.territoryCode).subscribe(response => {
-            this.listingCounts = response;
-       })
-    }
+export class ListingsComponent implements OnInit, OnChanges {
+  loaded = false;
+  listingCounts!: IListingCounts;
+  @Input() territoryCode = '';
+  @Output() loading = new EventEmitter<boolean>();
+
+  constructor(private router: Router, private listingService: ListingService) {
+  }
+
+  redirect() {
+    this.router.navigateByUrl('/new-listing')
+  }
+
+  ngOnInit() {
+    this.listingService.getListings('All').subscribe(response => {
+      this.listingCounts = response;
+      this.loading.emit(false);
+      this.loaded = true
+    })
+  }
+
+  ngOnChanges() {
+    this.listingService.getListings(this.territoryCode).subscribe(response => {
+      this.listingCounts = response;
+      this.loading.emit(false);
+      this.loaded = true
+    })
+  }
 
 
 }
--- apps/campground-booking/src/app/home/cards/orders.component.ts
+++ apps/campground-booking/src/app/home/cards/orders.component.ts
@@ -1,29 +1,39 @@
-import { Component,Input,OnChanges,OnInit } from "@angular/core";
+import { Component, Input, Output, OnChanges, OnInit, EventEmitter } from "@angular/core";
 import { Router } from "@angular/router";
 import { IOrdersCounts, } from '../../shared/orders-counts.model';
-import {OrdersService} from '../../shared/orders.service';
+import { OrdersService } from '../../shared/orders.service';
 
 @Component({
-    selector: 'orders',
-    templateUrl: './orders.component.html'
+  selector: 'orders',
+  templateUrl: './orders.component.html'
 })
-export class OrdersComponent implements OnInit, OnChanges {
-    ordersCounts!:IOrdersCounts;
-    @Input() territoryCode: string = '';
-    constructor(private router: Router, private ordersService:OrdersService) {
-    }
-    redirect() {
-        this.router.navigateByUrl('/new-listing')
-    }
-    ngOnInit(){
-        this.ordersService.getOrdersCounts('All').subscribe((response: IOrdersCounts) => {
-            this.ordersCounts = response;
-        });
-    }
 
-    ngOnChanges() {
-        this.ordersService.getOrdersCounts(this.territoryCode).subscribe((response: IOrdersCounts) => {
-            this.ordersCounts = response;
-       })
-    }
+export class OrdersComponent implements OnInit, OnChanges {
+  ordersCounts!: IOrdersCounts;
+  @Input() territoryCode = '';
+  @Output() loading = new EventEmitter<boolean>();
+  loaded = false
+
+  constructor(private router: Router, private ordersService: OrdersService) {
+  }
+
+  redirect() {
+    this.router.navigateByUrl('/new-listing')
+  }
+
+  ngOnInit() {
+    this.ordersService.getOrdersCounts('All').subscribe((response: IOrdersCounts) => {
+      this.ordersCounts = response;
+      this.loading.emit(false);
+      this.loaded = true
+    });
+  }
+
+  ngOnChanges() {
+    this.ordersService.getOrdersCounts(this.territoryCode).subscribe((response: IOrdersCounts) => {
+      this.ordersCounts = response;
+      this.loading.emit(false);
+      this.loaded = true
+    })
+  }
 }
