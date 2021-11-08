c9f2c21ce3c271fb11b9cb6a6d447d9e3835a000
FranklinLima7 <franklin.lima@aptitive.com>
2021-11-08 14:35:46 -0300

    fix: proposals module fix

--- apps/campground-booking/src/app/proposal/proposal.module.ts
+++ apps/campground-booking/src/app/proposal/proposal.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from "@angular/core";
 import { ListingModule } from "../listing/listing.module";
 import { CreateProposalComponent } from "./create-proposal/create-proposal.component";
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-    declarations: [  
-        CreateProposalComponent      
-      ],
-      imports: [
-          ListingModule
-      ],
-      schemas: [],
-      providers: []
+  declarations: [
+    CreateProposalComponent
+  ],
+  imports: [
+    ListingModule,
+    CommonModule,
+    BrowserModule
+  ],
+  schemas: [],
+  providers: []
 })
 
 export class ProposalModule { }
